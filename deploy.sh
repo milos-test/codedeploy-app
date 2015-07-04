7286162c53d5ed5aea3386787c1fd407a4a792d4
test <test@test.com>
2015-07-04 13:17:04 +0000

    zxsvfcvfcds

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git push origin master
 APP="test"
 DG="test"
 COMMIT="$(git log | head -n1 | awk '{print $2}')"
-REPO="https://github.com/milos-test/codedeploy-app"
+REPO="milos-test/codedeploy-app"
 
 aws deploy create-deployment \
    --application-name "$APP" \
